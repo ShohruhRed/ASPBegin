f41d17338fe83ba034df1d18682d56d68b8536ef
Shohruh Abduvohidov <shohruhred@inbox.ru>
2021-12-24 23:33:57 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPBegin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
